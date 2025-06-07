0dafec4deb388743ac19cb2cbde0f3b5c83f3a43
jonathan choi <jchoi4497@gmail.com>
2025-06-06 19:00:46 -0700

    cleaned up code

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,7 +5,7 @@ import DarkMode from "./DarkMode";
 function Main() {
   return (
     <div className="relative font-roboto bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300">
-      <div className="p-4 sm:block flex justify-between items-center">
+      <div className="p-3 sm:block">
         <DarkMode />
       </div>
       <Sidebar />
