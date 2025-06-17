46b1951741d566abac53095f5eb66819b773bff5
jonathan choi <jchoi4497@gmail.com>
2025-06-17 13:33:28 -0700

    bg white to sky less strain to eyes

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,7 +4,7 @@ import DarkMode from "./DarkMode";
 
 function Main() {
   return (
-    <div className="relative font-roboto bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300">
+    <div className="relative font-roboto bg-sky-100 text-black dark:bg-blue-950 dark:text-white transition-colors duration-300">
       <div className="p-3 sm:block">
         <DarkMode />
       </div>
